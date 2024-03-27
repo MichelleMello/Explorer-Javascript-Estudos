d6715d37ac63dbdcb99c96ee3dc96105df0b0ef1
Michelle Mello <mello.de.michelle@gmail.com>
2024-03-27 16:16:14 -0300

    Testing functions in Javascript

--- functions.js
+++ functions.js
@@ -17,4 +17,52 @@ function createPhrase() {
 //executar a função - execute, run, call, invoke
 
 createPhrase();
-console.log("End of the program");
+console.log("End of the motivational app");
+
+//function expression - function anonymous
+
+//parâmetros (parameters)
+const sum = function(number1, number2) {
+    total = number1 + number2;
+    return total;
+}
+
+let number1 = 34;
+let number2 = 25;
+
+//arguments - argumentos
+//sum(number1,number2); 
+
+console.log(`The sum is ${sum(number1,number2)}. Number 1 is ${number1} and number 2 is ${number2}.`);
+
+console.log("End of the sum function");
+
+//Função é um liquidificador
+
+function fazerSuco(fruta1, fruta2) {
+    return fruta1 + fruta2;
+}
+
+const copo = fazerSuco("banana", "maçã");
+
+console.log(copo);
+
+//Function scope
+let subject = "create video";
+
+function createThink(subject) {
+    return subject = "study";
+}
+
+console.log(subject);
+createThink();
+console.log(subject);
+
+
+//Function hoisting - elevação
+
+sayMyName();
+
+function sayMyName() {
+    console.log("Michelle");
+}
