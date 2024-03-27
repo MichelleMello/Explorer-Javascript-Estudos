3eef81787e35943cb97aa62db6a882cbab04f624
Michelle Mello <mello.de.michelle@gmail.com>
2024-03-27 15:12:20 -0300

    Created a function

---
+++ functions.js
@@ -0,0 +1,20 @@
+// Criar um app de frases motivacionais
+
+//função para repetir as frases
+
+//declaration - declaração da função
+//function statement 
+
+function createPhrase() {
+    //novo escopo aberto pela function
+
+    console.log("Keep going!");
+    console.log("Don't give up!");
+    console.log("You can do it!");
+
+}
+
+//executar a função - execute, run, call, invoke
+
+createPhrase();
+console.log("End of the program");
